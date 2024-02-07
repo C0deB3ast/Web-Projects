32b6683c795047979d077ca6df9265b668d9540d
C0deB3ast <37912nishu.nk@gmail.com>
2024-02-07 09:34:21 +0530

    Errorhandle

--- E-Comm/Backend/config/database.js
+++ E-Comm/Backend/config/database.js
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 const connectDB =()=>{
     mongoose.connect(process.env.DATABASE).then((data)=>{
         console.log(`DAtAbase connected with server: ${data.connection.host}`);
-       }).catch((errr)=>{
-           console.log(errr);
        })
 }
 
 export default connectDB
+
+//What is Mongoose?
+//Mongoose is an ODM (Object Data Modeling) library for MongoDB and Node.js. It provides a straightforward, schema-based solution for modeling application data and includes features such as validation, casting, and business logic hooks.
--- E-Comm/Backend/middleware/error.js
+++ E-Comm/Backend/middleware/error.js
@@ -5,6 +5,12 @@ export const errorMiddleware = (err,req,res,next)=>{
     err.statusCode = err.statusCode || 500;
     err.message = err.message || "Internal Server Error";
 
+    //Wrong Mongodb Id error
+    if(err.name === "CastError"){
+        const messsage = `Resource not found. Invalid: ${err.path}`;
+        err = new ErrorHandeler(messsage,400)
+    }
+
     res.status(err.statusCode).json({
         success:false,
         message:err.message
