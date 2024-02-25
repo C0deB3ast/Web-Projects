763c310b7d6a59cc241655a2b3d418657a11256a
C0deB3ast <37912nishu.nk@gmail.com>
2024-02-25 12:35:03 +0530

    USer

--- E-Comm/Backend/app.js
+++ E-Comm/Backend/app.js
@@ -1,18 +1,17 @@
 import express from "express";
-import {router as product} from "./routes/productRoutes.js"
+import { router as product } from "./routes/productRoutes.js";
+import { router as user } from "./routes/userRoutes.js";
 import { errorMiddleware } from "./middleware/error.js";
 const app = express();
 
-app.use(express.json())
-//Routes import
+app.use(express.json());
 //const product = require("./routes/productRoutes");
 
-
 app.use("/api/v1", product);
+app.use("/api/v1", user);
 
 // Middleware for error
 
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
-
-export default app
+export default app;
--- E-Comm/Backend/controllers/userController.js
+++ E-Comm/Backend/controllers/userController.js
@@ -1,3 +1,24 @@
 import User from "../models/usermodel.js";
 import { catchAsyncErrors } from "../middleware/cacthAsyncErrors.js";
 import ErrorHandeler from "../utils/errorHandler.js";
+
+//Register User
+
+export const registerUser = catchAsyncErrors(async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: "this is a sample id",
+      url: "profileurl",
+    },
+  });
+
+  res.status(201).json({
+    success: true,
+    user,
+  });
+});
