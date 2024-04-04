a38445c5667690e177c2497293a51596cd1c3377
C0deB3ast <37912nishu.nk@gmail.com>
2024-04-05 01:26:46 +0530

    CRUD is Done

--- Ecomm/Backend/controllers/productController.js
+++ Ecomm/Backend/controllers/productController.js
@@ -1,3 +1,73 @@
-export function getAllProducts(req,res){
-    res.status(200).json({message:"Route is working awosme"})
-}
+import Product from "../models/productModel.js";
+
+//Create Product-Admin
+export const createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
+
+//Get All Products
+export const getAllProducts = async (req, res) => {
+  const products = await Product.find();
+  res.status(200).json({
+    success: true,
+    products,
+  });
+};
+
+//Get Single Product
+export const getSingleProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    res.status(500).json({
+      success: false,
+      message: "Product Not Found",
+    });
+  }
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+//Update Product
+
+export const updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    res.status(500).json({
+      success: false,
+      message: "Product Not Found",
+    });
+  }
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+//Delete Product
+export const deleteProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    res.status(500).json({
+      success: false,
+      message: "Product Not Found",
+    });
+  }
+  await product.deleteOne();
+
+  res.status(200).json({
+    success: true,
+    message: "Product Deleted",
+  });
+};
--- Ecomm/Backend/routes/productRoute.js
+++ Ecomm/Backend/routes/productRoute.js
@@ -1,8 +1,16 @@
 import express  from "express";
-import { getAllProducts } from "../controllers/productController.js";
+import { getAllProducts,createProduct, updateProduct, deleteProduct, getSingleProduct } from "../controllers/productController.js";
 
 const router = express.Router();
 
+//Create Product
+router.route("/product/new").post(createProduct);
+
+//Get All Products
 router.route("/products").get(getAllProducts);
+
+//Update Product
+router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getSingleProduct);
+
 
 export default router
